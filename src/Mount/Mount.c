f32b705307f282464e97ca023644067f573bd190
lollolong <65832054+lollolong@users.noreply.github.com>
2023-10-06 13:00:00 +0200

    extend mount drop down menu with mount with options

--- src/Mount/Mount.c
+++ src/Mount/Mount.c
@@ -491,12 +491,13 @@ static void DisableSplitButton(HWND hwndDlg, int buttonId)
 	}
 }
 
-static HMENU CreateMountNoCacheDropdownMenu()
+static HMENU CreateMountDropdownMenu()
 {
 	HMENU hmenu = CreatePopupMenu();
 
 	// add menu items
 	AppendMenu(hmenu, MF_STRING, IDM_MOUNIT_NO_CACHE, GetString("IDM_MOUNT_NO_CACHE"));
+	AppendMenu(hmenu, MF_STRING, IDM_MOUNT_VOLUME_OPTIONS, GetString("IDM_MOUNT_VOLUME_OPTIONS"));
 
 	return hmenu;
 }
@@ -8347,7 +8348,7 @@ BOOL CALLBACK MainDialogProc (HWND hwndDlg, UINT uMsg, WPARAM wParam, LPARAM lPa
 			if (pnmh->idFrom == IDOK && pnmh->code == BCN_DROPDOWN)
 			{
 				// Create a popup menu for the split button
-				HMENU hmenu = CreateMountNoCacheDropdownMenu();
+				HMENU hmenu = CreateMountDropdownMenu();
 				HandleMountButtonDropdown(pnmh->hwndFrom, hwndDlg, hmenu);
 				DestroyMenu(hmenu);
 			}
